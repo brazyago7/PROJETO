9e4561a9b0fa43730072811ff5d64aa6261fb34e
brazyago7 <brazyago7@gmail.com>
2025-08-20 17:59:49 -0300

    Add initial HTML, CSS, and JavaScript files for Mario Bros project

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const form = document.querySelector(".fale-conosco")
+const mask = document.querySelector(".mascara-formulario")
+
+function cliqueinobotao(){
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mask.style.visibility = "visible"
+}
+function cliqueimascra(){
+    mask.style.visibility = "hidden"
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+}
